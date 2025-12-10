84b4cfc71f173f5539316e923fbffc5e391d4d32
Eleonora Khlebus <111703232+ekhlebus@users.noreply.github.com>
2025-12-10 23:40:07 +0000

    Simple web service created

---
+++ 05-deploying_ML_model/ping.py
@@ -0,0 +1,17 @@
+# Here we are turning ping function into a web service using Flask
+
+from flask import Flask
+
+app = Flask('ping')
+
+# Add decorator to add extra functionality to ping function (here it allows us to turn function into web service)
+@app.route('/ping', methods=['GET'])
+
+# Making ping function
+def ping():
+    return "PONG"
+
+# We are using "__main__" top-level script environment to run the app
+if __name__ == "__main__":
+    # Run the app on port 9696 at localhost
+    app.run(debug=True, host='0.0.0.0', port=9696) 
